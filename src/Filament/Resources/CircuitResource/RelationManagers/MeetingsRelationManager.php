5a785b2de76c6159d515997eabde02561417db42
Michael Bishop <michael@bishop.net.za>
2025-05-02 09:33:19 +0200

    Filter fix

--- src/Filament/Resources/CircuitResource/RelationManagers/MeetingsRelationManager.php
+++ src/Filament/Resources/CircuitResource/RelationManagers/MeetingsRelationManager.php
@@ -8,7 +8,6 @@ use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
 use Filament\Tables\Filters\Filter;
-use Filament\Tables\Filters\Filter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
